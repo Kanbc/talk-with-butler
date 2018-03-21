072a531de1d0ea684b364f413f3167a42f995896
Pawarisson <kan.pawarisson@gmail.com>
2018-03-21 16:55:24 +0700

    fix min to sec

--- read_time.py
+++ read_time.py
@@ -3,7 +3,5 @@ from flask.json import jsonify
 def estimation(text,img_num):
     len_text = len(text)
     img_num = int(img_num)
-    read_time = float((len_text/1000) + img_num*0.2)
-    if read_time < 1:
-        return jsonify({'time': int(1), 'unit': 'min' })
-    return jsonify({'time': int(round(read_time)), 'unit': 'min' })
+    read_time = (len_text/1000)*60 + img_num*12
+    return jsonify({'time': read_time, 'unit': 'sec' })
